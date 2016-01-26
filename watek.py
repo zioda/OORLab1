d389d06bcb68dd7ef14bfdd172fb6e8823d0a2e7
Kamil Błaszczyk <Kamil Baszczyk>
2016-01-26 07:02:08 +0100

    wypch

---
+++ watek.py
@@ -0,0 +1,40 @@
+import threading
+import time
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self): #uruchomienie
+        print "Starting " + self.name
+        threadLock.acquire() 
+        print_time(self.name, self.counter, 3)
+        threadLock.release()
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print "%s: %s" % (threadName, time.ctime(time.time()))
+        counter -= 1
+threadLock = threading.Lock()
+threads = []
+
+
+thread1 = myThread (1,"Watek-1", 1)
+thread2 = myThread (2,"Watek-2", 2)
+
+
+thread1.start()
+thread2.start()
+
+
+threads.append(thread1)
+threads.append(thread2)
+
+
+for t in threads:
+    t.join()  
+print "Watek"
+
